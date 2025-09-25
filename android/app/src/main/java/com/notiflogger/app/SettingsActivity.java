45e87f9da70537f880369b2caf211cb9d5b36163
lp85d <lp85d@mail.ru>
2025-09-25 16:23:57 +0400

    Update ActivationManager, SettingsActivity with ViewParent import, and build.yml

--- android/app/src/main/java/com/notiflogger/app/SettingsActivity.java
+++ android/app/src/main/java/com/notiflogger/app/SettingsActivity.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.view.ViewParent;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
